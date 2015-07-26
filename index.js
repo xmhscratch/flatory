de614cc511e0d853d61a569c77b999380c8524af
xmhscratch <xmhscratch@gmail.com>
2015-07-26 16:13:03 +0700

    update

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ module.exports = function(fspath) {
 	fspath = path.join(process.cwd(), fspath);
 	fspath = path.normalize(fspath);
 
-	var stats = fs.statSync(fspath);
+	if (/\.[^\/\\.]+$/g.test(fspath)) {
+		return new File(fspath);
+	}else{
+		return new Directory(fspath);
+	}
 
-	if (stats.isDirectory()) return new Directory(fspath);
-	else if(stats.isFile()) return new File(fspath);
-	else return;
+	return false;
 }
