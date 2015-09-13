65b7bdc792a6b1aa46f8613f009b381bb20d5f4c
xmhscratch <xmhscratch@gmail.com>
2015-09-13 23:36:25 +0700

    update

--- index.js
+++ index.js
@@ -1,52 +1,10 @@
-var fs = require('fs');
-var path = require('path');
-var mkdirp = require('mkdirp');
-
-var Directory = require('./lib/directory');
-var File = require('./lib/file');
-
-var ITEM_TYPE_DIRECTORY = 0;
-var ITEM_TYPE_FILE = 1;
-var ITEM_TYPE_SYMLINK = 2;
-
-function toAbsolutePath(fspath) {
-	var absPath = (false === path.isAbsolute(fspath))
-		? path.join(process.cwd(), fspath)
-		: fspath;
-	return path.normalize(absPath);
-}
+var Item = require('./lib/item');
 
 module.exports = function(root, type) {
-	var item;
-	root = toAbsolutePath(root);
-	try {
-		fs.accessSync(root);
-		var stat = fs.statSync(root);
-
-		if (stat.isDirectory()) {
-			item = new Directory(root, stat);
-		}else if(stat.isFile()) {
-			item = new File(root, stat);
-		}else if(stat.isSymbolicLink()) {
-			return;
-		}else{
-			return false;
-		}
-	}catch(e) {
-		type = (type || (/\.[^.]+$/g.test(root) ? ITEM_TYPE_FILE : ITEM_TYPE_DIRECTORY));
-		switch(type) {
-			case ITEM_TYPE_DIRECTORY: {
-				mkdirp.sync(root);
-				item = new Directory(root);
-				return;
-			}
-			default: return false;
-		}
-	}
-
-	return item;
+	var item = new Item(root);
+	return item.toInstance(type);
 }
 
-module.exports.TYPE_DIRECTORY = ITEM_TYPE_DIRECTORY;
-module.exports.TYPE_FILE = ITEM_TYPE_FILE;
-module.exports.TYPE_SYMLINK = ITEM_TYPE_SYMLINK;
+module.exports.TYPE_DIRECTORY = Item.TYPE_DIRECTORY;
+module.exports.TYPE_FILE = Item.TYPE_FILE;
+module.exports.TYPE_SYMLINK = Item.TYPE_SYMLINK;
