8e0f3a66932e27cbe604cc6f88429c1ca02edf30
xmhscratch <xmhscratch@gmail.com>
2015-09-13 19:02:48 +0700

    add test and update functions

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
 var fs = require('fs');
 var path = require('path');
+var mkdirp = require('mkdirp');
 
 var Directory = require('./lib/directory');
 var File = require('./lib/file');
 
-module.exports = function(fspath) {
-	fspath = (false === path.isAbsolute(fspath))
+var ITEM_TYPE_DIRECTORY = 0;
+var ITEM_TYPE_FILE = 1;
+var ITEM_TYPE_SYMLINK = 2;
+
+function toAbsolutePath(fspath) {
+	var absPath = (false === path.isAbsolute(fspath))
 		? path.join(process.cwd(), fspath)
 		: fspath;
-	fspath = path.normalize(fspath);
-
-	try	{
-		fs.accessSync(fspath);
-		var stat = fs.statSync(fspath);
-
-		if (stat.isFile()) {
-			return new File(fspath, stat);
-		}else if(stat.isDirectory()) {
-			return new Directory(fspath, stat);
-		}
-	}catch(e) {}
-
-	return false;
+	return path.normalize(absPath);
 }
+
+module.exports = function(root, type) {
+	var item;
+	root = toAbsolutePath(root);
+	try {
+		fs.accessSync(root);
+		var stat = fs.statSync(root);
+
+		if (stat.isDirectory()) {
+			item = new Directory(root, stat);
+		}else if(stat.isFile()) {
+			item = new File(root, stat);
+		}else if(stat.isSymbolicLink()) {
+			return;
+		}else{
+			return false;
+		}
+	}catch(e) {
+		type = (type || (/\.[^.]+$/g.test(root) ? ITEM_TYPE_FILE : ITEM_TYPE_DIRECTORY));
+		switch(type) {
+			case ITEM_TYPE_DIRECTORY: {
+				mkdirp.sync(root);
+				item = new Directory(root);
+				return;
+			}
+			default: return false;
+		}
+	}
+
+	return item;
+}
+
+module.exports.TYPE_DIRECTORY = ITEM_TYPE_DIRECTORY;
+module.exports.TYPE_FILE = ITEM_TYPE_FILE;
+module.exports.TYPE_SYMLINK = ITEM_TYPE_SYMLINK;
---
+++ test/specs/directorySpec.js
@@ -0,0 +1,19 @@
+var __ = require('../../index');
+var path = require('path');
+var fs = require('fs');
+var rootdir = path.join(__dirname, '../root');
+
+describe("scan item", function() {
+
+	var root = __(rootdir);
+
+	it("synchronous ensure testdir ensure folder exist", function() {
+		var testdir = path.join(rootdir, 'ensure-test');
+		var test = __(testdir);
+
+		// fs.access(testdir, fs.R_OK, function(error) {
+		// 	if (error) return expect(test.ensure()).toEqual(testdir);
+		// 	return expect(test.ensure()).toBeNull();
+		// });
+	});
+});
