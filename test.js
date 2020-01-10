9288c07542023f27048b23cde1dafbc543e167ac
xmhscratch <xmhscratch@gmail.com>
2020-01-10 07:34:42 -0600

    update

--- test.js
+++ test.js
@@ -5,28 +5,59 @@ var path = require('path')
 var __ = require('./')
 var rootDir = path.join(__dirname, './test/root')
 
+// console.log(
+// 	__(rootDir, 'app', 'components').getFullPath(),
+// 	__(rootDir, 'app', 'components').getStat(),
+// )
+
 // __(rootDir, 'app', 'components')
-//  .move(path.join(rootDir))
+//  .moveSync(path.join(rootDir))
 // __(rootDir, 'components')
-//  .move(path.join(rootDir, 'app'))
+//  .moveSync(path.join(rootDir, 'app'))
 // __(rootDir, 'app', 'components')
-//  .moveAsync(path.join(rootDir), _.noop)
+//  .move(path.join(rootDir), _.noop)
 // __(rootDir, 'components')
-//  .moveAsync(path.join(rootDir, 'app'), _.noop)
+//  .move(path.join(rootDir, 'app'), _.noop)
 
 // __(rootDir, 'app', 'components', 'blog', 'blogController.js')
-//  .move(path.join(rootDir, 'app'))
+//  .moveSync(path.join(rootDir, 'app'))
 // __(rootDir, 'app', 'blogController.js')
-//  .move(path.join(rootDir, 'app', 'components', 'blog'))
+//  .moveSync(path.join(rootDir, 'app', 'components', 'blog'))
 // __(rootDir, 'app', 'components', 'blog', 'blogController.js')
-//     .moveAsync(path.join(rootDir, 'app'))
+//     .move(path.join(rootDir, 'app'))
 // __(rootDir, 'app', 'blogController.js')
-//     .moveAsync(path.join(rootDir, 'app', 'components', 'blog'))
+//     .move(path.join(rootDir, 'app', 'components', 'blog'))
 
 // __(rootDir, 'app', 'components', 'blog', 'blogController.js')
-//  .rename('blogController1.js')
+//  .renameSync('blogController1.js')
+// __(rootDir, 'app', 'components', 'blog', 'blogController1.js')
+//  .renameSync('blogController.js')
 // __(rootDir, 'app', 'components', 'blog', 'blogController.js')
-//  .renameAsync('blogController1.js', _.noop)
+//  .rename('blogController1.js', _.noop)
+// __(rootDir, 'app', 'components', 'blog', 'blogController1.js')
+//  .rename('blogController.js', _.noop)
 
-__(rootDir, '5847.js')
-	.writeAsync('4445345345', _.noop)
+// __(rootDir, '5847.js')
+// 	.write('111', _.noop)
+// __(rootDir, '5847.js')
+// 	.writeSync('222')
+
+// __(rootDir, '5847.js')
+// 	.read({ encoding: 'utf8' }, console.log)
+// console.log(
+// 	__(rootDir, '5847.js')
+// 		.readSync({ encoding: 'utf8' }),
+// )
+
+// __(rootDir, '5847.js')
+// 	.delete({}, console.log)
+// console.log(
+// 	__(rootDir, '5847.js')
+// 		.deleteSync({})
+// )
+// __(rootDir, 'app', 'components')
+// 	.delete({}, console.log)
+// console.log(
+// 	__(rootDir, 'app', 'components')
+// 		.deleteSync({})
+// )
